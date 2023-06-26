45121eede3ddcec3d24673f3997979d4dd48c59f
ajsalsalim <ajsalsalim251@gmail.com>
2023-06-26 10:37:39 +0530

    first commit

---
+++ my-react-app/src/components/header.js
@@ -0,0 +1,4 @@
+function Header(){
+    return(<h2>HEADING</h2>)
+}
+// export default Header;
---
+++ my-react-app/src/counter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function counter({word,count}) {
+    
+    return (
+        <div>
+            
+       
+       <h1>{word}{count}</h1>
+            
+        </div>
+    )
+}
+
+export default counter
